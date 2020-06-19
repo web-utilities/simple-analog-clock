70f2f51bf2ac971d6d58523ca232bab634155946
Anirudh <anirudhganwal06@gmail.com>
2020-06-19 22:23:47 +0530

    made responsive designs

--- canvas.js
+++ canvas.js
@@ -1,11 +1,18 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const CANVAS_WIDTH = 300;
-const CANVAS_HEIGHT = 300;
+// dimentions
+const CANVAS_SIZE = 500;
 
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
+// colors
+const LABEL_COLOR = '#000';
+const DIAL_COLOR = '#eb3474';
+const SECOND_COLOR = '#eb3474';
+const MINUTE_COLOR = '#000';
+const HOUR_COLOR = '#000';
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
 class Clock {
 	draw = () => {
@@ -16,21 +23,21 @@ class Clock {
 		const minute = time.getMinutes();
 		const second = time.getSeconds();
 
-		ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-		ctx.translate(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+		ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+		ctx.translate(CANVAS_SIZE / 2, CANVAS_SIZE / 2);
 
 		ctx.save();
 
 		// Labels
-		ctx.lineWidth = 5;
-		ctx.strokeStyle = '#000';
+		ctx.lineWidth = CANVAS_SIZE * 0.01;
+		ctx.strokeStyle = LABEL_COLOR;
 		for (let i = 0; i < 60; i++) {
 			ctx.beginPath();
-			ctx.moveTo(0, -CANVAS_WIDTH / 2 + 15);
+			ctx.moveTo(0, -CANVAS_SIZE * 0.48 + 5);
 			if (i % 5 == 0) {
-				ctx.lineTo(0, -CANVAS_WIDTH / 2 + 40);
+				ctx.lineTo(0, -CANVAS_SIZE * 0.45 + 10);
 			} else {
-				ctx.lineTo(0, -CANVAS_WIDTH / 2 + 20);
+				ctx.lineTo(0, -CANVAS_SIZE * 0.47 + 4);
 			}
 			ctx.stroke();
 			ctx.rotate(Math.PI * 2 / 60);
@@ -41,9 +48,9 @@ class Clock {
 
 		// Outer circle of clock
 		ctx.beginPath();
-		ctx.lineWidth = 10;
-		ctx.strokeStyle = '#eb3474';
-		ctx.arc(0, 0, CANVAS_WIDTH / 2 - 5, 0, Math.PI * 2, false);
+		ctx.lineWidth = CANVAS_SIZE * 0.02;
+		ctx.strokeStyle = DIAL_COLOR;
+		ctx.arc(0, 0, CANVAS_SIZE / 2 - 5, 0, Math.PI * 2, false);
 		ctx.stroke();
 
 		ctx.restore();
@@ -51,11 +58,11 @@ class Clock {
 
 		// Hour needle
 		ctx.beginPath();
-		ctx.lineWidth = 10;
-		ctx.strokeStyle = '#000';
+		ctx.lineWidth = CANVAS_SIZE * 0.02;
+		ctx.strokeStyle = HOUR_COLOR;
 		ctx.rotate(Math.PI * 2 / 12 * (hour + minute / 60 + second / 3600));
-		ctx.moveTo(0, 15);
-		ctx.lineTo(0, -80);
+		ctx.moveTo(0, CANVAS_SIZE * 0.04);
+		ctx.lineTo(0, -CANVAS_SIZE * 0.25);
 		ctx.stroke();
 
 		ctx.restore();
@@ -63,11 +70,11 @@ class Clock {
 
 		// Minute clock
 		ctx.beginPath();
-		ctx.lineWidth = 5;
-		ctx.strokeStyle = '#000';
+		ctx.lineWidth = CANVAS_SIZE * 0.01;
+		ctx.strokeStyle = MINUTE_COLOR;
 		ctx.rotate(Math.PI * 2 / 60 * (minute + second / 60));
-		ctx.moveTo(0, 15);
-		ctx.lineTo(0, -100);
+		ctx.moveTo(0, CANVAS_SIZE * 0.04);
+		ctx.lineTo(0, -CANVAS_SIZE * 0.38);
 		ctx.stroke();
 
 		ctx.restore();
@@ -75,11 +82,11 @@ class Clock {
 
 		// Second clock
 		ctx.beginPath();
-		ctx.lineWidth = 2;
-		ctx.strokeStyle = '#eb3474';
+		ctx.lineWidth = CANVAS_SIZE * 0.003 + 1;
+		ctx.strokeStyle = SECOND_COLOR;
 		ctx.rotate(Math.PI * 2 / 60 * second);
-		ctx.moveTo(0, 20);
-		ctx.lineTo(0, -120);
+		ctx.moveTo(0, CANVAS_SIZE * 0.07);
+		ctx.lineTo(0, -CANVAS_SIZE * 0.42);
 		ctx.stroke();
 
 		ctx.restore();
@@ -87,8 +94,8 @@ class Clock {
 
 		// Center Dot
 		ctx.beginPath();
-		ctx.fillStyle = '#eb3474';
-		ctx.arc(0, 0, 7, 0, Math.PI * 2, false);
+		ctx.fillStyle = SECOND_COLOR;
+		ctx.arc(0, 0, CANVAS_SIZE * 0.02, 0, Math.PI * 2, false);
 		ctx.fill();
 
 		ctx.restore();
