09ac8987ad0933604cabd0524beaf4afc3ac48d2
Anirudh <anirudhganwal06@gmail.com>
2020-06-19 20:39:40 +0530

    added centre dot and clock labels

--- canvas.js
+++ canvas.js
@@ -21,6 +21,24 @@ class Clock {
 
 		ctx.save();
 
+		// Labels
+		ctx.lineWidth = 5;
+		ctx.strokeStyle = '#000';
+		for (let i = 0; i < 60; i++) {
+			ctx.beginPath();
+			ctx.moveTo(0, -CANVAS_WIDTH / 2 + 15);
+			if (i % 5 == 0) {
+				ctx.lineTo(0, -CANVAS_WIDTH / 2 + 40);
+			} else {
+				ctx.lineTo(0, -CANVAS_WIDTH / 2 + 20);
+			}
+			ctx.stroke();
+			ctx.rotate(Math.PI * 2 / 60);
+		}
+
+		ctx.restore();
+		ctx.save();
+
 		// Outer circle of clock
 		ctx.beginPath();
 		ctx.lineWidth = 10;
@@ -36,7 +54,7 @@ class Clock {
 		ctx.lineWidth = 10;
 		ctx.strokeStyle = '#000';
 		ctx.rotate(Math.PI * 2 / 12 * (hour + minute / 60 + second / 3600));
-		ctx.moveTo(0, 10);
+		ctx.moveTo(0, 15);
 		ctx.lineTo(0, -80);
 		ctx.stroke();
 
@@ -48,7 +66,7 @@ class Clock {
 		ctx.lineWidth = 5;
 		ctx.strokeStyle = '#000';
 		ctx.rotate(Math.PI * 2 / 60 * (minute + second / 60));
-		ctx.moveTo(0, 10);
+		ctx.moveTo(0, 15);
 		ctx.lineTo(0, -100);
 		ctx.stroke();
 
@@ -60,9 +78,18 @@ class Clock {
 		ctx.lineWidth = 2;
 		ctx.strokeStyle = '#eb3474';
 		ctx.rotate(Math.PI * 2 / 60 * second);
-		ctx.moveTo(0, 10);
+		ctx.moveTo(0, 20);
 		ctx.lineTo(0, -120);
 		ctx.stroke();
+
+		ctx.restore();
+		ctx.save();
+
+		// Center Dot
+		ctx.beginPath();
+		ctx.fillStyle = '#eb3474';
+		ctx.arc(0, 0, 7, 0, Math.PI * 2, false);
+		ctx.fill();
 
 		ctx.restore();
 
